7defb23bd106f6542b48eb69a9d54bb5726074e6
Nate D <nate@qstreet.org>
2022-04-27 14:52:50 +0300

    Make all inputs the same height and variable quality

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const createImage = async () => {
   const width = localStorage.getItem("width")
   const height = localStorage.getItem("height")
 
+  const findQuality = () => {
+    switch (localStorage.getItem("quality")) {
+      case "l":
+        return 64
+      case "m":
+        return 300
+      case "h":
+        return 640
+    }
+  }
+
+  const quality = findQuality()
+
   const response = await fetch(
     `https://api.spotify.com/v1/me/top/tracks?limit=${width * height}`,
     {
@@ -14,16 +27,24 @@ const createImage = async () => {
   const items = (await response.json()).items
 
   const canvas = document.querySelector("canvas")
-  canvas.width = width * 300
-  canvas.height = height * 300
+  canvas.width = width * quality
+  canvas.height = height * quality
 
   items.forEach((item, i) => {
     const img = document.createElement("img")
-    img.src = item.album.images[1].url
+
+    img.src = item.album.images.filter(
+      (albumImage) => albumImage.height === quality
+    )[0].url
+
     img.onload = () => {
       canvas
         .getContext("2d")
-        .drawImage(img, (i % width) * 300, ((i - (i % width)) / width) * 300)
+        .drawImage(
+          img,
+          (i % width) * quality,
+          ((i - (i % width)) / width) * quality
+        )
     }
   })
 }
@@ -35,11 +56,10 @@ if (location.hash) {
   createImage()
 }
 
-document.querySelector("form").onsubmit = (e) => {
-  e.preventDefault()
-
+document.querySelector("button").onclick = () => {
   localStorage.setItem("width", document.getElementById("width").value)
   localStorage.setItem("height", document.getElementById("height").value)
+  localStorage.setItem("quality", document.querySelector("select").value)
 
   location.href =
     "https://accounts.spotify.com/authorize?" +
